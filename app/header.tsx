f0b30449cb86f180e2db0fd85375e5800860cef7
Taeil <102219209+k1k2brz@users.noreply.github.com>
2022-12-04 00:05:54 +0900

    git 연습 및 css 수정

--- app/header.tsx
+++ app/header.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const header = () => {
     return (
-        <div className="p-5 bg-blue-500">header</div>
+        <div className="p-5 bg-blue-500">
+            <p className="font-bold text-white">Front Header</p>
+        </div>
     )
 }
 

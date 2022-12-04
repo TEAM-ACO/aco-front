bed6d72a31bea6502df4b39bcf390e93c8ea438e
Taeil <102219209+k1k2brz@users.noreply.github.com>
2022-12-04 14:39:54 +0900

    Next13 실험 및 git 연습

---
+++ app/LogIn/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from './LoginForm'
+
+function LogIn() {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default LogIn
---
+++ app/SignUp/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignUp() {
+    return (
+        <div>SignUp Router Test</div>
+    )
+}
+
+export default SignUp

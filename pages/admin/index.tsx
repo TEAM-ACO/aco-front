c5117caf9cf364c1877331f907e2634d1f3dd033
Taeil <102219209+k1k2brz@users.noreply.github.com>
2022-12-11 00:28:55 +0900

    content, comment report, delete modal

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -3,7 +3,21 @@ import AdminPage from './AdminPage'
 
 const admin = () => {
     return (
-        <div>
+        <div className=''>
+            <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+                <li className="mr-2">
+                    <a href="#" aria-current="page" className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">전체 보기</a>
+                </li>
+                <li className="mr-2">
+                    <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">유저 관리</a>
+                </li>
+                <li className="mr-2">
+                    <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">신고 관리</a>
+                </li>
+                <li className="mr-2">
+                    <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">방문자 분석</a>
+                </li>
+            </ul>
             <AdminPage />
         </div>
     )
--- pages/mainpage/PostCard.tsx
+++ pages/mainpage/PostCard.tsx
@@ -1,18 +1,15 @@
 import { IComments, IPost } from '@features/postSlice';
 import { useAppSelector } from '@store/config';
 import dayjs from 'dayjs';
+import { Avatar } from 'flowbite-react';
 import React, { useState, useCallback } from 'react'
 import CommentForm from './CommentForm';
 import CommentList from './CommentList';
+import Dropdown from './Dropdown';
 import PostCardContent from './PostCardContent';
 
 function PostCard({ post }: any) {
     const { isLoading } = useAppSelector((state) => state.post);
-    const [postCardDropdown, setPostCardDropdown] = useState<boolean>(true);
-
-    const onTogglePostCardDropdown = useCallback(() => {
-        setPostCardDropdown((prev) => !prev)
-    }, [])
 
     return (
         <>
@@ -22,54 +19,25 @@ function PostCard({ post }: any) {
                         <img className='object-cover' src={post.articleImage} alt='img' />
                     </div>
                     <div className="px-6 py-4">
-                        <div>
-                            <button
-                                id="dropdownMenuIconHorizontalButton"
-                                data-dropdown-toggle="dropdownDotsHorizontal"
-                                className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                                type="button"
-                                onClick={onTogglePostCardDropdown}
-                            >
-                                <svg
-                                    className="w-6 h-6"
-                                    aria-hidden="true"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                                </svg>
-                            </button>
-                            <div className={postCardDropdown ? 'hidden' : 'flex'}>
-                                <div
-                                    id="dropdownDotsHorizontal"
-                                    className="z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul
-                                        className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                        aria-labelledby="dropdownMenuIconHorizontalButton">
-                                        <li>
-                                            <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                                        </li>
-                                    </ul>
-                                    <div className="py-1">
-                                        <button
-                                            className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                                            게시글 삭제
-                                        </button>
-                                    </div>
-                                </div>
+                        {/* 게시글 Dropdown */}
+                        <div className='flex justify-between mb-3'>
+                            <div className='flex justify-start items-center'>
+                                {/* 누르면 프로필로 가게 할 것 */}
+                                <button
+                                    className='flex justify-start items-center'>
+                                    <Avatar
+                                        img="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                                        rounded={true}
+                                    />
+                                    <p className='ml-2'>
+                                        {post.writer}
+                                    </p>
+                                </button>
                             </div>
+                            <Dropdown />
                         </div>
                         <div>
-                            {post.writer}
-                        </div>
-                        <div>
-                            <div className="font-bold text-xl mb-2">{post.title}</div>
+                            <p className="font-bold text-xl mb-2">{post.title}</p>
                         </div>
                         <div>
                             <p className="text-gray-700 text-base">
@@ -79,19 +47,22 @@ function PostCard({ post }: any) {
                     </div>
                     {/* HASHTAG */}
                     <PostCardContent />
+                    {/* Comment Input */}
                     <CommentForm post={post} />
                     <div>
-                        <ol className=" border-l border-gray-200 dark:border-gray-700">
-                            <div className='ml-6 py-2 text-xs'>
+                        <div className=" border-l border-gray-200 dark:border-gray-700">
+                            <div className='ml-6 py-2 mt-3 text-xs text-cyan-800'>
                                 {`${post.Comments.length}개의 댓글`}
                             </div>
-                            {/* 만약 CommentList로 뺀게 불편하다면 말씀해주세요. */}
-                            {post.Comments.map((post: any) => {
-                                return (
-                                    <CommentList key={post.User.nickname} comment={post} />
-                                )
-                            })}
-                        </ol>
+                            <div className='mt-2'>
+                                {/* 만약 CommentList로 뺀게 불편하다면 말씀해주세요. */}
+                                {post.Comments.map((post: any) => {
+                                    return (
+                                        <CommentList key={post.User.nickname} comment={post} />
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>

8edbfde6102be9bf87ecaf41c7abf0d8fc92fcfe
Taeil <102219209+k1k2brz@users.noreply.github.com>
2023-02-02 20:49:48 +0900

    lightsail url

--- config/url.ts
+++ config/url.ts
@@ -1,2 +1,2 @@
-export const backendURL = process.env.NODE_ENV === 'production' ? './' : 'http://localhost:3075/'
+export const backendURL = process.env.NODE_ENV === 'production' ? 'http://43.200.152.148:3075/' : 'http://localhost:3075/'
 export default backendURL;
